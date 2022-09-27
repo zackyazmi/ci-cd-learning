e783d64e9e7e22940e2730da8aba97a47185a875
Zacky Ilahi Azmi <zackyilahi.azmi@gmail.com>
2022-09-27 14:58:00 +0700

    Initial Commit

---
+++ ci cd learningUITests/ci_cd_learningUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ci_cd_learningUITestsLaunchTests.swift
+//  ci cd learningUITests
+//
+//  Created by Zacky Ilahi Azmi on 27/09/22.
+//
+
+import XCTest
+
+final class ci_cd_learningUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
